c8877bffac354faea4ec1c52b906a829da14023c
Stache <stache@stache.cat>
2024-02-19 19:34:56 +0100

    Working PoC

---
+++ app.py
@@ -0,0 +1,314 @@
+import asyncio
+import os
+import time
+
+import arxiv
+from dash import Dash, html, dcc, callback, Output, Input, State, dash_table, DiskcacheManager, CeleryManager
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+from dash.exceptions import PreventUpdate
+from scholarly import scholarly
+
+from scrapper import SCRAPPER_COLUMNS, prep_expression, expression_to_arxiv_query, expression_to_scolar_query, COOLDOWN, \
+    NUM_RETRIES
+from test_data import TEST_DATA
+
+if 'REDIS_URL' in os.environ:
+    # Use Redis & Celery if REDIS_URL set as an env variable
+    from celery import Celery
+
+    celery_app = Celery(__name__, broker=os.environ['REDIS_URL'], backend=os.environ['REDIS_URL'])
+    background_callback_manager = CeleryManager(celery_app)
+
+else:
+    # Diskcache for non-production apps when developing locally
+    import diskcache
+
+    cache = diskcache.Cache("./cache")
+    background_callback_manager = DiskcacheManager(cache)
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+           background_callback_manager=background_callback_manager)
+
+controls = dbc.Card([
+    dbc.CardBody(dbc.Form(
+        [
+            html.Div(
+                [
+                    dbc.Label("Keywords to include (AND, OR)", html_for="keywords_include"),
+                    dbc.Input(id="keywords_include",
+                              placeholder="e.g., \"(Machine Learning) OR (Apprentissage Automatisé)\"", type="text"),
+                ]
+            ),
+            html.Div(
+                [
+                    dbc.Label("Keywords to exclude (NOT)", html_for="keywords_exclude"),
+                    dbc.Input(id="keywords_exclude", placeholder="e.g., \"Deep Learning\"", type="text"),
+                ]
+            ),
+            html.Div(
+                [
+                    dbc.Label("Platforms", html_for="platforms"),
+                    dbc.Checklist(
+                        options=[
+                            {"label": "arXiv", "value": "arxiv"},
+                            {"label": "Google Scholar", "value": "scholar"},
+                        ],
+                        value=[],
+                        id="platforms",
+                        inline=True,
+                        switch=True,
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    dbc.Label("Number of results (per platform)", html_for="max_results"),
+                    dbc.Input(id="max_results", type="number", value=100, min=0, step=1),
+                ]
+            ),
+            html.Div([
+                dbc.Button("Cancel", id="cancel_button", color="secondary", className="mt-2 me-2 disabled"),
+                dbc.Button("Search", id="search_button", color="success", className="mt-2"),
+            ], className="float-end")
+        ],
+    )),
+    dbc.CardFooter([
+        html.Dl(id="infos", className="row"),
+        dbc.Progress(id="progress", animated=True, striped=True, style={"visibility": "hidden"}),
+    ]),
+]
+)
+
+table = dash_table.DataTable(
+    id="datatable",
+    data=None,  # Dynamic
+    columns=[{"name": v, "id": k} for k, v in SCRAPPER_COLUMNS.items()],
+    style_cell={
+        "overflow": "hidden",
+        "textOverflow": "ellipsis",
+        "maxWidth": 0,
+    },
+    tooltip_data=None,  # Dynamic
+    tooltip_duration=None,
+    filter_action="native",
+    sort_action="native",
+    sort_mode="multi",
+    row_selectable="multi",
+    selected_columns=[],
+    selected_rows=[],
+    page_action="native",
+    page_current=0,
+    page_size=10,
+)
+
+app.layout = dbc.Container(
+    [
+        html.H1("Gratte-Papier"),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col(controls, md=4),
+                dbc.Col(table, md=8),
+            ],
+            align="center",
+        ),
+        dcc.Interval(id="interval", disabled=True),
+        dcc.Store(id="data_store"),
+    ],
+    fluid=True,
+)
+
+
+def human_time(seconds):
+    if seconds <= 0:
+        return "Terminé !"
+    if seconds < 60:
+        return "Moins d'une minute"
+    else:
+        minutes = round(seconds / 60)
+        if minutes < 60:
+            return f"Environ {minutes} minute{'s' if minutes != 1 else ''}"
+        else:
+            hours = round(minutes / 60)
+            return f"Environ {hours} heure{'s' if hours != 1 else ''}"
+
+
+@app.callback(
+    Output("search_button", "n_clicks"),
+    Output("infos", "children"),
+    Output("data_store", "data"),
+    Input("search_button", "n_clicks"),
+    Input("keywords_include", "value"),
+    Input("keywords_exclude", "value"),
+    Input("platforms", "value"),
+    Input("max_results", "value"),
+)
+def start_scrapping(n, included, excluded, platforms, max_results):
+    if n and included and platforms and max_results > 0:
+        children = [
+            html.Dt("Entrées trouvées", className="col-4"),
+            html.Dd("0", id="found", className="col-8"),
+        ]
+        data = {}
+
+        included = included.lower() if included else ""
+        excluded = " ".join(
+            [f" not {word}" for word in excluded.lower().replace("not", " ").split(" ")]) if excluded else ""
+
+        expression = prep_expression(included + excluded)
+        children += [
+            html.Dt("Expression", className="col-4"),
+            html.Dd(html.Pre(expression), id="expression", className="col-8"),
+        ]
+        data["expression"] = expression
+
+        if "arxiv" in platforms:
+            arxiv_query = expression_to_arxiv_query(expression)
+            children += [
+                html.Dt("Requête arXiv", className="col-4"),
+                html.Dd(html.Pre(arxiv_query), id="r_arxiv", className="col-8"),
+            ]
+            data["arxiv_query"] = arxiv_query
+
+        if "scholar" in platforms:
+            scolar_query = expression_to_scolar_query(expression)
+            children += [
+                html.Dt("Requête Google Scholar", className="col-4"),
+                html.Dd(html.Pre(scolar_query), id="r_scholar", className="col-8"),
+            ]
+            data["scolar_query"] = scolar_query
+
+        max_time = len(platforms) * max_results
+        children += [
+            html.Dt("Temps estimé", className="col-4"),
+            html.Dd(human_time(max_time), id="estimated", className="col-8"),
+        ]
+        data["max_results"] = max_results
+        data["max_time"] = max_time
+
+        return 0, children, data
+    else:
+        raise PreventUpdate
+
+
+@app.callback(
+    Output("datatable", "data"),
+    Output("datatable", "tooltip_data"),
+    Input("data_store", "data"),
+    background=True,
+    running=[
+        (Output("search_button", "disabled"), True, False),
+        (Output("cancel_button", "disabled"), False, True),
+        (
+                Output("progress", "style"),
+                {"visibility": "visible"},
+                {"visibility": "hidden"},
+        )
+    ],
+    cancel=Input("cancel_button", "n_clicks"),
+    progress=[
+        Output("progress", "value"),
+        Output("progress", "max"),
+        Output("found", "children"),
+        Output("estimated", "children"),
+    ],
+    prevent_initial_call=True,
+)
+def process_scrapping(set_progress, data):
+    arxiv_query = data["arxiv_query"] if "arxiv_query" in data else None
+    scolar_query = data["scolar_query"] if "scolar_query" in data else None
+    max_results = data["max_results"] if "max_results" in data else None
+    cur_est_total = max_results * ((arxiv_query is not None) + (scolar_query is not None))
+    cur_est_time = cur_est_total * ((arxiv_query is not None) + (scolar_query is not None))
+
+    df = pd.DataFrame()
+
+    if arxiv_query:
+        client = arxiv.Client(num_retries=NUM_RETRIES)
+        search = arxiv.Search(query=arxiv_query, max_results=max_results)
+        arxiv_gen = client.results(search)
+
+        for i, result in enumerate(arxiv_gen):
+            next_row = pd.Series({
+                "doi": f"10.48550/arXiv.{result.entry_id.split('/')[-1].split('v')[0]}",
+                "year": result.published.year,
+                "title": result.title,
+                "authors": ";".join(str(author) for author in result.authors),
+                "abstract": result.summary,
+                "journal": result.journal_ref,
+                "url_pdf": [link.href for link in result.links if link.title == "pdf"][0],
+                "url_others": ";".join(
+                    [f"{link.title or 'canonical'}:{link.href}" for link in result.links if link.title != "pdf"]
+                ),
+                "from": "arXiv",
+            }).to_frame().T
+            df = pd.concat([df, next_row], ignore_index=True)
+            cur_len = len(df.index)
+            cur_est_time -= 1
+
+            if not (i + 1) % COOLDOWN:
+                set_progress((
+                    str(cur_len),
+                    str(cur_est_total),
+                    str(cur_len),
+                    str(human_time(cur_est_time)),
+                ))
+                time.sleep(COOLDOWN)
+
+            if i == max_results - 1:
+                break
+
+    cur_est_total = len(df.index) + max_results * (scolar_query is not None)
+
+    if scolar_query:
+        scolar_gen = scholarly.search_pubs(scolar_query)
+
+        for i, result in enumerate(scolar_gen):
+            next_row = pd.Series({
+                "doi": None,  # No DOI on Google Scholar
+                "year": result.bib.get("year", None),
+                "title": result.bib.get("title", None),
+                "authors": ";".join(result.bib.get("author", None)),
+                "abstract": result.bib.get("abstract", None),
+                "journal": result.bib.get("venue", None),
+                "url_pdf": result.bib.get("eprint", None),
+                "url_others": f"canonical:https://scholar.google.com{result.citations_link}:;source:{result.bib.get('url', None)}",
+                "from": "Google Scholar",
+            }).to_frame().T
+            df = pd.concat([df, next_row], ignore_index=True)
+            cur_len = len(df.index)
+            cur_est_time -= 1
+
+            if not (i + 1) % COOLDOWN:
+                set_progress((
+                    str(cur_len),
+                    str(cur_est_total),
+                    str(cur_len),
+                    str(human_time(cur_est_time)),
+                ))
+                time.sleep(COOLDOWN)
+
+            if i == max_results - 1:
+                break
+
+    set_progress((
+        str(len(df.index)),
+        str(len(df.index)),
+        str(len(df.index)),
+        str(human_time(0)),
+    ))
+
+    tooltip_data = [
+        {
+            column: {"value": str(value), "type": "markdown"}
+            for column, value in row.items()
+        } for row in df.to_dict("records")
+    ]
+
+    return df.to_dict(orient="records"), tooltip_data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ scrapper.py
@@ -0,0 +1,128 @@
+import arxiv
+from scholarly import scholarly
+import pandas as pd
+import re
+import boolean
+import string
+from tqdm.auto import tqdm
+import asyncio
+
+COOLDOWN = 5
+NUM_RETRIES = 10
+MAX_RESULTS = 100
+SCRAPPER_COLUMNS = {
+    "doi": "DOI",
+    "year": "Year",
+    "title": "Title",
+    "authors": "Authors",
+    "abstract": "Abstract",
+    "journal": "Journal",
+    "url_pdf": "URL to PDF",
+    "url_others": "Other URLs",
+    "from": "Source",
+}
+
+
+def prep_expression(request):
+    # NOT > AND > OR
+    text = " ".join(request.lower().replace(" -", " and not ").replace("\"", "'").split()) + " "
+
+    for q in re.findall(r"(\'.*?\')", text):
+        t = q[:-1].replace("'", "").replace(" ", "_")
+        text = text.replace(q, t) + " "
+
+    for p in string.punctuation:
+        if p != "(" and p != ")":
+            text = text.replace(p, "_")
+
+    text = text.replace(" and ", "&").replace(" or ", "|").replace("not ", "~").strip().replace(" ", "&")
+
+    algebra = boolean.BooleanAlgebra()
+
+    expression = algebra.parse(text, simplify=True)
+
+    return str(expression)
+
+
+def expression_to_arxiv_query(expression):
+    for word in set(re.findall(r"(\w+)", expression)):
+        expression = expression.replace(word, f"all:{word}")
+
+    expression = expression \
+        .replace("&~", " ANDNOT ") \
+        .replace("~", " ANDNOT ") \
+        .replace("&", " AND ") \
+        .replace("|", " OR ") \
+        .replace("_", " ")
+
+    return expression
+
+
+def expression_to_scolar_query(expression):
+    for word in set(re.findall(r"(\w+)", expression)):
+        expression = expression.replace(word, f"\"{word.replace('_', ' ')}\"")
+
+    expression = expression \
+        .replace("&~", " -") \
+        .replace("~", " -") \
+        .replace("&", " ") \
+        .replace("|", " OR ")
+
+    return expression
+
+
+async def arxiv_query_to_dataframe(query, max_results=MAX_RESULTS, num_retries=NUM_RETRIES, cooldown=COOLDOWN):
+    client = arxiv.Client(num_retries=num_retries)
+    search = arxiv.Search(query=query, max_results=max_results)
+    results = client.results(search)
+
+    df = pd.DataFrame()
+
+    for i, result in enumerate(tqdm(results, desc="arxiv")):
+        next_row = pd.Series({
+            "doi": f"10.48550/arXiv.{result.entry_id.split('/')[-1].split('v')[0]}",
+            "year": result.published.year,
+            "title": result.title,
+            "authors": result.authors,
+            "abstract": result.summary,
+            "journal": result.journal_ref,
+            "url_pdf": [link.href for link in result.links if link.title == "pdf"][0],
+            "url_others": [{link.title or "canonical": link.href for link in result.links if link.title != "pdf"}],
+            "from": "arXiv",
+        }).to_frame().T
+        df = pd.concat([df, next_row], ignore_index=True)
+        if not i % cooldown:
+            await asyncio.sleep(cooldown)
+        if i == max_results - 1:
+            break
+
+    yield df
+
+
+async def scolar_query_to_dataframe(query, max_results=MAX_RESULTS, num_retries=NUM_RETRIES, cooldown=COOLDOWN):
+    results = scholarly.search_pubs(query)
+
+    df = pd.DataFrame()
+
+    for i, result in enumerate(tqdm(results, desc="scolar")):
+        next_row = pd.Series({
+            "doi": None,  # No DOI on Google Scholar
+            "year": result.bib.get("year", None),
+            "title": result.bib.get("title", None),
+            "authors": result.bib.get("author", None),
+            "abstract": result.bib.get("abstract", None),
+            "journal": result.bib.get("venue", None),
+            "url_pdf": result.bib.get("eprint", None),
+            "url_others": [{
+                "canonical": f"https://scholar.google.com{result.citations_link}",
+                "source": result.bib.get("url", None)
+            }],
+            "from": "Google Scholar",
+        }).to_frame().T
+        df = pd.concat([df, next_row], ignore_index=True)
+        if not i % 10:
+            await asyncio.sleep(cooldown)
+        if i == max_results - 1:
+            break
+
+    yield df
